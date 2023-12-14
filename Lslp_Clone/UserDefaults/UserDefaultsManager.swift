9c47f2ee879861f2d4e69bc4c5c6cba79fd656b4
yeomsungpil <present7256@gmail.com>
2023-12-14 21:50:04 +0900

    UserDefaults 코드 정리

--- Lslp_Clone/UserDefaults/UserDefaultsManager.swift
+++ Lslp_Clone/UserDefaults/UserDefaultsManager.swift
@@ -18,34 +18,7 @@ class UserDefaultsManager {
     func saveRefreshToken(refreshToken: String) {
         UserDefaults.standard.set(refreshToken, forKey: "refreshToken")
     }
-    
-    func saveUserID(_ id: String) {
-        UserDefaults.standard.set(id, forKey: "UserID")
-    }
-    
-    func loadUserID() -> String {
-        
-        guard let userid = UserDefaults.standard.string(forKey: "UserID") else { return "" }
-        
-        return userid
-    }
-    
-    // 로그아웃 하거나 토큰이 만료되어 로그인뷰로 갔을때 초기화
-    
-    // 로그인 버튼 눌렀을 때  다시 id값 저장
-
-    
-    func saveNickname(_ nick: String) {
-        UserDefaults.standard.set(nick, forKey: "nickname")
-    }
-    
-    func loadNickname() -> String {
-        guard let nickname = UserDefaults.standard.string(forKey: "nickname") else {
-            return ""
-        }
-        return nickname
-    }
-
+ 
     func backToRoot(isRoot: Bool = false) {
         UserDefaults.standard.set(isRoot, forKey: "isVaild")
     }
@@ -53,13 +26,7 @@ class UserDefaultsManager {
     func backToCall() -> Bool {
         return UserDefaults.standard.bool(forKey: "isVaild")
     }
-    
-    
-    func refreshToAccessToken(token: RefreshResponse) {
-        UserDefaults.standard.set(token.token, forKey: "accessToken")
-    }
-
-    
+ 
     var accessToken: String {
         UserDefaults.standard.string(forKey: "accessToken") ?? "엑세스 토큰 없음"
     }
